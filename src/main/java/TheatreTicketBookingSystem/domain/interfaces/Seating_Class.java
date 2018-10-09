96000bfdadd8e31a2eb8d16e33401b8ea24816a2
Hasan <hasan.hoossen@yellowtail.co.za>
2018-10-09 16:02:04 +0200

    first Commit

---
+++ src/main/java/TheatreTicketBookingSystem/domain/interfaces/Seating_Class.java
@@ -0,0 +1,10 @@
+package TheatreTicketBookingSystem.domain.interfaces;
+
+/**
+ * Created by Hasan on 10/9/2018.
+ */
+public interface Seating_Class {
+    public String getClass_name();
+    public int getPrice();
+    public Long getSeat_id();
+}
